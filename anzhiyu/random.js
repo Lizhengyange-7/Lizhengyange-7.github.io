92218fff30af9751c81df5fabcde0d1af1bee3a6
Lizhengyange-7 <a577872771@163.com>
2025-02-27 18:40:25 +0800

    Site updated: 2025-02-27 18:40:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/27/摘抄/","2025/02/27/hello-world/","2025/02/27/诗歌/"];function toRandomPost(){
+var posts=["2025/02/27/hello-world/","2025/02/27/诗歌/","2025/02/27/摘抄/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
